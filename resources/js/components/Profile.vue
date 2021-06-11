e5bd935775845aa659c677acd181845194898c14
Johnny <johnny.liu@student.ehb.be>
2021-06-11 19:44:30 +0200

    changes

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -115,7 +115,7 @@ export default {
         ]),
         getProfilePicture() {
             if (this.profile.length !== 0 && this.profile[0].file !== null) {
-                return '~/workspace/storage/app/public/' + this.profile[0].file
+                return '../storage/profiles/' + this.profile[0].file
             }
         }
     },
